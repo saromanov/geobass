037105e0e42d983030b1cb0fe5e7d9bb6baf4a8d
Sergey <xxsmotur@gmail.com>
2019-01-26 02:58:14 +0500

    init of the app

---
+++ geobass.go
@@ -0,0 +1,30 @@
+package geobass
+
+import (
+	"sync"
+	"time"
+)
+
+// GeoBass defines app
+type GeoBass struct {
+	m sync.RWMutex
+}
+
+// Point defines point on store
+type Point struct {
+	Latitude  float64
+	Longitude float64
+}
+
+// Set provides setting to the cache
+func (c *GeoBass) Set(position Point, value interface{}, expiration time.Duration) {
+	c.m.Lock()
+	defer c.m.Unlock()
+}
+
+// Get provides getting object by the point
+func (c *GeoBass) Get(p Point) (interface{}, error) {
+	c.m.RLock()
+	defer c.m.RUnlock()
+	return nil, nil
+}
